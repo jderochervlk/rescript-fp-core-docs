beda222021caf5e14c90adc55c01c4c882dce5e9
Josh Vlk <josh@vlkpack.com>
2023-08-31 10:37:10 -0400

    array docs

--- src/layouts/LandingPageLayout.mjs
+++ src/layouts/LandingPageLayout.mjs
@@ -2,21 +2,45 @@
 
 import * as Mdx from "../common/Mdx.mjs";
 import * as Meta from "../components/Meta.mjs";
+import * as Next from "../bindings/Next.mjs";
 import * as React from "react";
+import * as Button from "../components/Button.mjs";
 import * as Markdown from "../components/Markdown.mjs";
 import * as Navigation from "../components/Navigation.mjs";
 import * as Caml_option from "rescript/lib/es6/caml_option.js";
 
+var text = [
+  "\n  Heavily inspired by fp-ts this library is an alternative to the ReScript Core library.\n  ReScript Core has a focus of providing bindings for JavaScript API's without having an opinion on those APIs.\n  This library has a different philosophy of providing functions that are focused on functional patterns, \n  and only offering a subset of Javascript APIs.\n  ",
+  "\n  This library encorages immutable data and safe access that are often wrapped in Options.\n  "
+];
+
 function LandingPageLayout$Intro(Props) {
-  return React.createElement("section", {
-              className: "flex justify-center"
-            }, React.createElement("div", {
-                  className: "max-w-1060 flex flex-col items-center px-5 sm:px-8 lg:box-content"
-                }, React.createElement("h1", {
-                      className: "hl-title text-center max-w-[53rem]"
-                    }, "rescript-fp-core"), React.createElement("h2", {
-                      className: "body-lg text-center text-gray-60 my-4 max-w-[40rem]"
-                    }, "A functional standard library for ReScript.")));
+  return React.createElement(React.Fragment, {
+              children: null
+            }, React.createElement("section", {
+                  className: "flex justify-center"
+                }, React.createElement("div", {
+                      className: "max-w-1060 flex flex-col items-center px-5 sm:px-8 lg:box-content"
+                    }, React.createElement("h1", {
+                          className: "hl-title text-center max-w-[53rem]"
+                        }, "rescript-fp-core"), React.createElement("h2", {
+                          className: "body-lg text-center text-gray-60 my-4 max-w-[40rem]"
+                        }, "A functional standard library for ReScript."), React.createElement("div", {
+                          className: "mt-4 mb-2"
+                        }, React.createElement(Next.Link.make, {
+                              href: "/docs/manual/installation",
+                              passHref: true,
+                              children: React.createElement(Button.make, {
+                                    children: "Get started"
+                                  })
+                            })))), React.createElement("section", {
+                  className: "container mx-auto mt-10 max-w-800"
+                }, text.map(function (t) {
+                      return React.createElement("p", {
+                                  key: t,
+                                  className: "mt-1"
+                                }, t);
+                    })));
 }
 
 function LandingPageLayout(Props) {
--- src/layouts/LandingPageLayout.res
+++ src/layouts/LandingPageLayout.res
@@ -1,19 +1,38 @@
+let text = [
+  `
+  Heavily inspired by fp-ts this library is an alternative to the ReScript Core library.
+  ReScript Core has a focus of providing bindings for JavaScript API's without having an opinion on those APIs.
+  This library has a different philosophy of providing functions that are focused on functional patterns, 
+  and only offering a subset of Javascript APIs.
+  `,
+  `
+  This library encorages immutable data and safe access that are often wrapped in Options.
+  `,
+]
+
 module Intro = {
   @react.component
   let make = () => {
-    <section className="flex justify-center">
-      <div className="max-w-1060 flex flex-col items-center px-5 sm:px-8 lg:box-content">
-        <h1 className="hl-title text-center max-w-[53rem]"> {React.string("rescript-fp-core")} </h1>
-        <h2 className="body-lg text-center text-gray-60 my-4 max-w-[40rem]">
-          {React.string(`A functional standard library for ReScript.`)}
-        </h2>
-        // <div className="mt-4 mb-2">
-        //   <Next.Link href="/docs/manual/latest/installation" passHref={true}>
-        //     <Button> {React.string("Get started")} </Button>
-        //   </Next.Link>
-        // </div>
-      </div>
-    </section>
+    <React.Fragment>
+      <section className="flex justify-center">
+        <div className="max-w-1060 flex flex-col items-center px-5 sm:px-8 lg:box-content">
+          <h1 className="hl-title text-center max-w-[53rem]">
+            {React.string("rescript-fp-core")}
+          </h1>
+          <h2 className="body-lg text-center text-gray-60 my-4 max-w-[40rem]">
+            {React.string(`A functional standard library for ReScript.`)}
+          </h2>
+          <div className="mt-4 mb-2">
+            <Next.Link href="/docs/manual/installation" passHref={true}>
+              <Button> {React.string("Get started")} </Button>
+            </Next.Link>
+          </div>
+        </div>
+      </section>
+      <section className="container mx-auto mt-10 max-w-800">
+        {text->Js.Array2.map(t => <p key={t} className="mt-1"> {React.string(t)} </p>)->React.array}
+      </section>
+    </React.Fragment>
   }
 }
 
